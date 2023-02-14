7126889b641b746ca1bcdd72c3ef1e2f34da2873
PLWilczynskiPL <66273956+PLWilczynskiPL@users.noreply.github.com>
2023-02-14 15:46:12 +0100

    Update redirect.js

--- projekty/js/redirect.js
+++ projekty/js/redirect.js
@@ -1,3 +1,4 @@
 if (location.protocol == 'https:') // detect https
   location.href = location.href.replace(/^https:/, 'http:') // change the protocol from https:/ to http:
 // credit: some guy on stackoverflow
+// modified by me
