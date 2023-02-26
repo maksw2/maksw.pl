3ce518f7f899819b14be723a0a2a170bdfaef83d
PLWilczynskiPL <makswilczynski2010@gmail.com>
2023-02-26 18:29:56 +0100

    modify

--- js/random.js
+++ js/random.js
@@ -1,8 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var imageArray = ["./images/image1.webp", "./images/image2.webp", "./images/image3.webp", "./images/image4.webp", "./images/image5.webp", "./images/image6.webp"];
-  var randomImageIndex = Math.floor(Math.random() * imageArray.length);
-  var randomSlide = document.getElementById("random-slide");
-  randomSlide.style.backgroundImage = "none";
-  var randomImage = document.getElementById("random-image");
-  randomImage.src = imageArray[randomImageIndex];
-});
+      document.addEventListener("DOMContentLoaded", function() {
+        var imageArray = ["./images/image1.webp", "./images/image2.webp", "./images/image3.webp", "./images/image4.webp", "./images/image5.webp", "./images/image6.webp"];
+        var randomImageIndex = Math.floor(Math.random() * imageArray.length);
+        var imageSrc = imageArray[randomImageIndex];
+        var slideElement = document.querySelector(".single-slide");
+        slideElement.style.backgroundImage = "url('" + imageSrc + "')";
+      });
