f82b430aa8f5c7c6a24f3e6a9b7a434147d518fd
PLWilczynskiPL <makswilczynski2010@gmail.com>
2023-03-01 09:36:26 +0100

    testting to see if it will load faster

--- js/redirect.js
+++
@@ -1,4 +0,0 @@
-if (location.protocol == 'http:') // detect http:
-  location.href = location.href.replace(/^http:/, 'https:') // change the protocol FROM http:/ TO https:
-// reverse (https to http) can be found in /projekty/js/redirect.js
-// credit: some guy on stackoverflow
