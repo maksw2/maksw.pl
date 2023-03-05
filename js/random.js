a0cedc47eebd1045dcf8a17d73fc5fddaef2af0d
PLWilczynskiPL <makswilczynski2010@gmail.com>
2023-03-05 17:52:08 +0100

    add some needed optimalizations and fixes

--- js/random.js
+++
@@ -1,7 +0,0 @@
-      document.addEventListener("DOMContentLoaded", function() {
-        var imageArray = ["./images/image1.webp", "./images/image2.webp", "./images/image3.webp", "./images/image4.webp", "./images/image5.webp", "./images/image6.webp"];
-        var randomImageIndex = Math.floor(Math.random() * imageArray.length);
-        var imageSrc = imageArray[randomImageIndex];
-        var slideElement = document.querySelector(".single-slide");
-        slideElement.style.backgroundImage = "url('" + imageSrc + "')";
-      });
